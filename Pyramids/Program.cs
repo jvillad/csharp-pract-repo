61ea87f158f312d8d16b76ffccc97b91873c09d1
supremevillad <106669923+supremevillad@users.noreply.github.com>
2022-08-27 13:15:09 +1000

    Add project files.

---
+++ Pyramids/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Please enter a number: ");
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int numOfStars))
+                {
+                    for (int i = 1; i <= numOfStars; i++)
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    for (int i = numOfStars; i > i * -1; i--)
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    for (int i = numOfStars; i > i * -1; i--)
+                    {
+                        for (int j = i; j < numOfStars; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int k = 1; k <= i; k++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    for (int i = 1; i <= numOfStars; i++)
+                    {
+                        for (int j = i; j < numOfStars; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int k = 1; k <= i; k++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+
+                }
+                else
+                    Console.WriteLine("Invalid number, Try again.");
+            }
+            
+
+        }
+    }
+}
---
+++ TempConvertion/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConvertion
+{
+    
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            //Exercise 1
+            CelciusToFahrenheit();
+            //Exercise 2
+            FahrenheitToCelcius();
+            //Exercise 3
+            while (true)
+            {
+                Console.WriteLine("Temperature Convertion \n 1. Celcius to Fahrenheit \n 2. Fahrenheit to Celcius");
+                Console.WriteLine("Please select the desired conversion"); 
+                char userSelection = Console.ReadKey(true).KeyChar;
+                
+                if (userSelection == '1')
+                {
+                    Console.WriteLine($"You pressed {userSelection} for Celcius to Fahrenheit");
+                    CelciusToFahrenheit();
+                }
+                else if (userSelection == '2')
+                {
+                    Console.WriteLine($"You pressed {userSelection} for Fahrenheit to Celcius");
+                    FahrenheitToCelcius();
+                }
+                else
+                    Console.WriteLine("\nInvalid selection, Please try again. ");
+            }
+        }
+        private static void CelciusToFahrenheit()
+        {            
+            Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+
+            if (double.TryParse(userInput, out double numCelcius))
+            {
+                Console.WriteLine($"You entered {userInput} degrees Celcius");
+                double fahrenheit = (numCelcius * 9 / 5) + 32;
+                Console.WriteLine($"That is equivalent to {fahrenheit} degrees Fahrenheit\n");
+            }
+            else
+                Console.WriteLine("Please enter a valid number");
+        }
+        private static void FahrenheitToCelcius()
+        {
+            Console.Write("Please enter a number: ");
+            string userInput = Console.ReadLine();
+
+            if (double.TryParse(userInput, out double numFahrenheit))
+            {
+                Console.WriteLine($"You entered {userInput} degrees Celcius");
+                double celcius = (numFahrenheit - 32) * 5 / 9;
+                Console.WriteLine($"That is equivalent to {celcius} degrees Celcius\n");
+            }
+            else
+                Console.WriteLine("Please enter a valid number");
+        }
+    }
+}
