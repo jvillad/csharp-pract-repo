88278fffc1d1f18daba59fe20e2f0fbec44d6a1f
johnrey.villademosa <jyvillad@gmail.com>
2023-06-29 18:10:45 +1000

    Added an Order class to track the Ordered Item.

---
+++ BrewMachina.PractInventoryManagement/BrewMachina.PractInventoryManagement/Domain/OrderManagement/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace BrewMachina.PractInventoryManagement.Domain.OrderManagement;
+
+public class Order
+{
+    public int Id { get; private set; }
+    public DateTime OrderFulfilmentDate { get; private set; }
+    public List<OrderItem> OrderItems { get; }
+    public bool Fulfilled { get; set; } = false;
+
+    public Order()
+    {
+        // random id value
+        Id = new Random().Next(999999);
+        
+        int numberofSeconds = new Random().Next(100);
+        OrderFulfilmentDate = DateTime.Now.AddSeconds(numberofSeconds);
+
+        OrderItems = new List<OrderItem>();
+    }
+}
