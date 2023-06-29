66ec7e51d942b8b48bb660bc09fa15fb12889652
johnrey.villademosa <jyvillad@gmail.com>
2023-06-29 18:09:31 +1000

    New enum currency and a composition between Product class and Price class.

---
+++ BrewMachina.PractInventoryManagement/BrewMachina.PractInventoryManagement/Domain/General/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace BrewMachina.PractInventoryManagement.Domain.General;
+
+public enum Currency
+{
+    Aud,
+    Php,
+    Usd
+}
---
+++ BrewMachina.PractInventoryManagement/BrewMachina.PractInventoryManagement/Domain/General/Price.cs
@@ -0,0 +1,12 @@
+﻿namespace BrewMachina.PractInventoryManagement.Domain.General;
+
+public class Price
+{
+    public double ItemPrice { get; set; }
+    public Currency Currency { get; set; }
+
+    public override string ToString()
+    {
+        return $"{ItemPrice} {Currency}";
+    }
+}
